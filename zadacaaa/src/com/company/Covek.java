c61044d8829662a919c514708ed2b7cc5537f68d
ACER <belkoskijovan111@gmail.com>
2021-11-29 15:30:10 +0100

    First Commit

---
+++ zadacaaa/src/com/company/Covek.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Covek {
+    String ime="Jovan";
+    String prezime="Belkoski";
+    int godini=21;
+
+    public Covek(){
+
+    }
+}
---
+++ zadacaaa/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Covek n=new Covek();
+    System.out.println(n.ime);
+    }
+}
